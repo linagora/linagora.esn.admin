a688242402f98373bc40a810bc90df8c08304f0e
Sang NGO DOAN <sngodoan@linagora.com>
2016-08-15 16:47:35 +0700

    ADM-29 Switch to new esn-config API

--- backend/webserver/api/configuration/controller.js
+++ backend/webserver/api/configuration/controller.js
@@ -1,33 +1,75 @@
 'use strict';
 
-var domainConfig;
+var q = require('q');
+var esnConfig;
+var logger;
 
 function getConfigurations(req, res) {
   var configNames = req.body.configNames;
-  var domainId = req.domain._id;
+  var user = req.user;
 
-  domainConfig.get(domainId, configNames)
-    .then(function(configs) {
-      return res.status(200).json(configs);
-    }, function(err) {
-      return res.json(500, {error: {code: 500, message: 'Server Error', details: err.message}});
+  if (!Array.isArray(configNames)) {
+    return res.status(400).json({
+      error: {
+        code: 400,
+        message: 'Bad Request',
+        details: 'configNames should be an array of configuration\'s name'
+      }
     });
+  }
+
+  q.all(configNames.map(function(configName) {
+    return esnConfig(configName)
+      .forUser(user)
+      .get()
+      .then(function(data) {
+        if (!data) {
+          return null;
+        }
+
+        return {
+          name: configName,
+          value: data
+        };
+      });
+  })).then(function(configs) {
+    return res.status(200).json(configs.filter(Boolean));
+  }, function(err) {
+    logger.error('Error while getting configuration:', err);
+
+    return res.status(500).json({error: {code: 500, message: 'Server Error', details: err.message}});
+  });
 }
 
 function updateConfigurations(req, res) {
-  var configs = req.body.configs || [];
+  var configs = req.body.configs;
   var domainId = req.domain._id;
 
-  domainConfig.set(domainId, configs)
-    .then(function(data) {
-      return res.status(200).json(data);
+  if (!Array.isArray(configs)) {
+    return res.status(400).json({
+      error: {
+        code: 400,
+        message: 'Bad Request',
+        details: 'configs should be an array of configuration to update'
+      }
+    });
+  }
+
+  var esnConf = new esnConfig.EsnConfig(null, domainId);
+
+  esnConf.setMultiple(configs)
+    .then(function() {
+      return res.status(200).json(configs);
     }, function(err) {
-      return res.json(500, {error: {code: 500, message: 'Server Error', details: err.message}});
+      logger.error('Error while updating configuration:', err);
+
+      return res.status(500).json({error: {code: 500, message: 'Server Error', details: err.message}});
     });
 }
 
 module.exports = function(dependencies) {
-  domainConfig = dependencies('domain-config');
+  esnConfig = dependencies('esn-config');
+  logger = dependencies('logger');
 
   return {
     getConfigurations: getConfigurations,
