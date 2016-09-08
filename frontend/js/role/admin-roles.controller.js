74c2da5cdd74172fbe208606f106dd67410ad46e
Cuong Nguyen Ngoc <ncnguyen@linagora.com>
2016-09-08 14:41:31 +0700

    ADM-61 Implement Admin Management page to display administrator list

---
+++ frontend/js/role/admin-roles.controller.js
@@ -0,0 +1,8 @@
+'use strict';
+
+angular.module('linagora.esn.admin')
+
+.controller('adminRolesController', function($stateParams) {
+  var self = this;
+  var domainId = $stateParams.domainId;
+});
