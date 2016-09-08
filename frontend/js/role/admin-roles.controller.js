935f3b7479d4581b93704ebf6118132d99632dd6
Cuong Nguyen Ngoc <ncnguyen@linagora.com>
2016-09-08 15:26:37 +0700

    ADM-61 Creating layout at frontend side

--- frontend/js/role/admin-roles.controller.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('linagora.esn.admin')
-
-.controller('adminRolesController', function($stateParams) {
-  var self = this;
-  var domainId = $stateParams.domainId;
-});
