febfa48269d30bba49c40897a788a0717ec26702
Cuong Nguyen Ngoc <ncnguyen@linagora.com>
2016-09-19 16:04:07 +0700

    ADM-61 Get the administrators data from server

--- frontend/js/roles/admin-roles.controller.js
+++ frontend/js/roles/admin-roles.controller.js
@@ -2,13 +2,11 @@
 
 angular.module('linagora.esn.admin')
 
-.controller('adminRolesController', function() {
+.controller('adminRolesController', function($stateParams, domainAPI) {
   var self = this;
+  var domainId = $stateParams.domainId;
 
-  self.administrators = [
-    {_id: 1, firstname: 'Cuong', lastname: 'Nguyen', emails: ['ngoccuong94@gmail.com', 'cuong94gmail.com'], promoted: '01/02/2016'},
-    {_id: 2, firstname: 'Nam', lastname: 'Nguyen', emails: ['namnguyen94@gmail.com', 'nam94gmail.com'], promoted: '01/02/2016'},
-    {_id: 3, firstname: 'Quang', lastname: 'Trinh', emails: ['quangtrinh94@gmail.com', 'quang94gmail.com'], promoted: '01/02/2016'},
-    {_id: 4, firstname: 'Thanh', lastname: 'Nguyen', emails: ['thanhnguyen94@gmail.com', 'thanh94gmail.com'], promoted: '01/02/2016'}
-  ];
+  domainAPI.getAdministrators(domainId).then(function(resp) {
+    self.administrators = resp.data;
+  });
 });
