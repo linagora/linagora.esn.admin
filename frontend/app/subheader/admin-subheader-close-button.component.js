4b99b974f8c0f8268eba8502d81167cac00582d1
Sang NGO DOAN <sngodoan@linagora.com>
2016-10-28 16:37:47 +0700

    ADM-106 Add back button in advanced user creation page

---
+++ frontend/app/subheader/admin-subheader-close-button.component.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('linagora.esn.admin')
+
+.component('adminSubheaderCloseButton', {
+  templateUrl: '/admin/app/subheader/admin-subheader-close-button',
+  bindings: {
+    backState: '@'
+  }
+});
