24b8baa8346c2c354c51eebd2a873295514e6561
Sang NGO DOAN <sngodoan@linagora.com>
2017-03-10 10:00:56 +0700

    ADM-158 Add test button to test sending request from OP to James

--- frontend/app/james/admin-james.controller.js
+++ frontend/app/james/admin-james.controller.js
@@ -2,19 +2,33 @@
 
 angular.module('linagora.esn.admin')
 
-.controller('adminJamesController', function($stateParams, adminDomainConfigService, asyncAction) {
+.controller('adminJamesController', function($stateParams, adminDomainConfigService, asyncAction, adminJamesClientProvider) {
   var self = this;
   var domainId = $stateParams.domainId;
   var CONFIG_NAME = 'james';
+
+  self.save = save;
+  self.test = test;
 
   adminDomainConfigService.get(domainId, CONFIG_NAME)
     .then(function(data) {
       self.config = data;
     });
 
-  self.save = function() {
+  function save() {
     return asyncAction('Modification of James settings', _saveConfiguration);
-  };
+  }
+
+  function test() {
+    adminJamesClientProvider
+      .get(domainId)
+      .then(function(jameClient) {
+        jameClient.listDomains();
+      })
+      .catch(function(err) {
+        console.log(111, err);
+      });
+  }
 
   function _saveConfiguration() {
     return adminDomainConfigService.set(domainId, CONFIG_NAME, self.config);
