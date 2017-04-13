3620a05aab21adbf7241cc3c2ec4972c3bfacf99
Dung T. Pham <dtpham@linagora.com>
2017-04-13 16:26:44 +0700

    ADM-166 Create a platform admin page in Administration module

---
+++ frontend/app/platform/admin-platform-panel-switch.component.js
@@ -0,0 +1,10 @@
+(function(angular) {
+  'use strict';
+
+  angular.module('linagora.esn.admin')
+
+  .component('adminPlatformPanelSwitch', {
+    templateUrl: '/admin/app/platform/admin-platform-panel-switch',
+    controller: 'adminPlatformPanelSwitchController'
+  });
+})(angular);
---
+++ frontend/app/platform/admin-platform.component.js
@@ -0,0 +1,9 @@
+(function(angular) {
+  'use strict';
+
+  angular.module('linagora.esn.admin')
+
+  .component('adminPlatform', {
+    templateUrl: '/admin/app/platform/admin-platform'
+  });
+})(angular);
