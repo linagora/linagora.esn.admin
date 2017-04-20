27177e31c6d2ec26faaebd108742f7e6feb329bb
Dung T. Pham <dtpham@linagora.com>
2017-04-20 15:24:30 +0700

    ADM-143 As a platform admin, I can configure Facebook OAuth settings

---
+++ frontend/app/oauth/facebook/admin-oauth-facebook.js
@@ -0,0 +1,12 @@
+(function(angular) {
+  'use strict';
+
+  angular.module('linagora.esn.admin')
+
+  .component('adminOauthFacebook', {
+    templateUrl: '/admin/app/oauth/facebook/admin-oauth-facebook',
+    bindings: {
+      config: '='
+    }
+  });
+})(angular);
