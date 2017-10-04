4f9dcca49e8eb57324f414b9cb2b9afa626918b6
Dang Dien PHAN <ddphan@linagora.com>
2017-10-04 09:19:41 +0700

    #211 - UX updates for Modules module.

--- frontend/app/modules/admin-modules.controller.js
+++ frontend/app/modules/admin-modules.controller.js
@@ -2,11 +2,13 @@
 
 angular.module('linagora.esn.admin')
 
-.controller('adminModulesController', function($stateParams, rejectWithErrorNotification, adminModulesService, adminDomainConfigService) {
+.controller('adminModulesController', function($stateParams, rejectWithErrorNotification, adminModulesService, adminDomainConfigService, ADMIN_LOADING_STATUS) {
   var self = this;
   var domainId = $stateParams.domainId;
   var HOMEPAGE_VALUE = 'unifiedinbox';
   var HOMEPAGE_KEY = 'homePage';
+
+  self.status = ADMIN_LOADING_STATUS.loading;
 
   adminDomainConfigService.get(domainId, HOMEPAGE_KEY).then(function(data) {
     self.homePage = data || HOMEPAGE_VALUE;
@@ -14,5 +16,9 @@ angular.module('linagora.esn.admin')
 
   adminModulesService.get(domainId).then(function(modules) {
     self.modules = modules;
+    self.status = ADMIN_LOADING_STATUS.loaded;
+  })
+  .catch(function() {
+    self.status = ADMIN_LOADING_STATUS.error;
   });
 });
