6702c2e8b0b728dd30e5ac5a75e7e2d312a45fa6
Dang Dien PHAN <ddphan@linagora.com>
2017-10-04 09:19:20 +0700

    #211 - UX updates for Mail module.

--- frontend/app/mail/admin-mail.controller.js
+++ frontend/app/mail/admin-mail.controller.js
@@ -4,18 +4,23 @@ angular.module('linagora.esn.admin')
 
 .constant('ADMIN_MAIL_TRANSPORT_TYPES', { local: 'Local', smtp: 'SMTP', gmail: 'Gmail' })
 
-.controller('adminMailController', function($stateParams, adminDomainConfigService, adminMailService, asyncAction, ADMIN_MAIL_TRANSPORT_TYPES, ADMIN_DEFAULT_NOTIFICATION_MESSAGES) {
+.controller('adminMailController', function($stateParams, adminDomainConfigService, adminMailService, asyncAction, ADMIN_MAIL_TRANSPORT_TYPES, ADMIN_DEFAULT_NOTIFICATION_MESSAGES, ADMIN_LOADING_STATUS) {
   var self = this;
   var domainId = $stateParams.domainId;
   var CONFIG_NAME = 'mail';
 
   self.transportTypes = ADMIN_MAIL_TRANSPORT_TYPES;
+  self.status = ADMIN_LOADING_STATUS.loading;
 
   adminDomainConfigService.get(domainId, CONFIG_NAME)
     .then(function(data) {
       self.config = data || {};
       self.config.resolvers = self.config.resolvers || {};
       self.transportType = adminMailService.getTransportType(self.config);
+      self.status = ADMIN_LOADING_STATUS.loaded;
+    })
+    .catch(function() {
+      self.status = ADMIN_LOADING_STATUS.error;
     });
 
   self.save = function() {
