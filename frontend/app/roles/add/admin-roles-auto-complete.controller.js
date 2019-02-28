838ecb01d633826c22d36dd8c5fdf733c5a26705
Dung T. Pham <dtpham@linagora.com>
2019-02-28 15:02:52 +0700

    #546 use excluding user feature in attendee searching for admin-roles-autocomplete component

--- frontend/app/roles/add/admin-roles-auto-complete.controller.js
+++ frontend/app/roles/add/admin-roles-auto-complete.controller.js
@@ -2,18 +2,35 @@
 
 angular.module('linagora.esn.admin')
 
-.controller('adminRolesAutoCompleteController', function($element, elementScrollService, adminRolesService, _) {
+.controller('adminRolesAutoCompleteController', function($element, elementScrollService, adminRolesService) {
   var self = this;
 
-  self.search = adminRolesService.searchAdministratorCandidates;
+  self.search = search;
+  self.onTagAdded = onTagAdded;
 
-  self.onTagAdding = function($tag) {
-    return _.filter(self.newAdministrators, function(tag) {
-      return angular.equals(tag.id, $tag.id);
+  init();
+
+  function init() {
+    adminRolesService.getAdministrators().then(function(administrators) {
+      self.excludes = administrators.map(function(administrator) {
+        return {
+          id: administrator.id,
+          objectType: 'user'
+        };
+      });
     });
-  };
+  }
 
-  self.onTagAdded = function() {
+  function search(query) {
+    return adminRolesService.searchAdministratorCandidates(query, self.excludes);
+  }
+
+  function onTagAdded($tag) {
+    self.excludes.push({
+      id: $tag.id,
+      objectType: $tag.objectType
+    });
+
     elementScrollService.autoScrollDown($element.find('div.tags'));
-  };
+  }
 });
