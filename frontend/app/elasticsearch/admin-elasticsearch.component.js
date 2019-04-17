aea4f0ed2236c292444ebc7a9b54ffdd14b17df1
Dung Dung PHAM <dtpham@linagora.com>
2019-04-17 12:00:45 +0700

    #556 Add a new page for elasticsearch host configuration

---
+++ frontend/app/elasticsearch/admin-elasticsearch.component.js
@@ -0,0 +1,10 @@
+(function() {
+  'use strict';
+
+  angular.module('linagora.esn.admin')
+
+  .component('adminElasticsearch', {
+    templateUrl: '/admin/app/elasticsearch/admin-elasticsearch.html',
+    controller: 'adminElasticsearchController'
+  });
+})();
