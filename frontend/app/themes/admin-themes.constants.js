844b1cd7eb4a300f76bde7e7bfc5e4652dd58931
Christophe HAMERLING <chamerling@linagora.com>
2019-04-17 12:01:19 +0200

    #557 Add missing color

--- frontend/app/themes/admin-themes.constants.js
+++ frontend/app/themes/admin-themes.constants.js
@@ -3,13 +3,14 @@
 
   angular.module('linagora.esn.admin')
     .constant('ADMIN_THEMES_COLOR_VARIABLES', [
-      {apiVariable: 'primaryColor', displayText: 'Primary color', default: '#2196F3'},
-      {apiVariable: 'secondaryColor', displayText: 'Secondary color', default: '#FFC107'},
-      {apiVariable: 'bodyBgColor', displayText: 'Background color', default: '#f7f7f7'},
-      {apiVariable: 'textColor', displayText: 'Text color', default: '#ffffff'}
+      { apiVariable: 'primaryColor', displayText: 'Primary color', default: '#2196F3' },
+      { apiVariable: 'accentColor', displayText: 'Accent color', default: '#FFC107' },
+      { apiVariable: 'bodyBgColor', displayText: 'Background color', default: '#f7f7f7' },
+      { apiVariable: 'textPrimaryColor', displayText: 'Text primary color', default: '#ffffff' },
+      { apiVariable: 'textColor', displayText: 'Text color', default: '#666' }
     ])
     .constant('ADMIN_THEMES_LOGO_VARIABLES', [
-      {apiVariable: 'desktop', displayText: 'Logo', default: '/images/white-logo.svg'},
-      {apiVariable: 'mobile', displayText: 'Mobile logo', default: '/images/white-logo.svg'}
+      { apiVariable: 'desktop', displayText: 'Logo', default: '/images/white-logo.svg' },
+      { apiVariable: 'mobile', displayText: 'Mobile logo', default: '/images/white-logo.svg' }
     ]);
 })(angular);
