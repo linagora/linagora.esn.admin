da2adc2a31f1e9d81b2e0fea5176bc7f275ea4e4
Dung Dung PHAM <dtpham@linagora.com>
2019-10-02 17:08:33 +0700

    linagora.esn.contact#515 Enable maintenance page in domain mode. Remove dynamic directive anchor for maintenance entries

--- frontend/app/maintenance/admin-maintenance.controller.spec.js
+++ frontend/app/maintenance/admin-maintenance.controller.spec.js
@@ -7,15 +7,16 @@ var expect = chai.expect;
 
 describe('The adminMaintenanceController', function() {
   var $controller, $rootScope, $scope;
-  var adminMaintenanceService;
+  var adminMaintenanceService, adminModeService;
 
   beforeEach(function() {
     module('linagora.esn.admin');
 
-    inject(function(_$controller_, _$rootScope_, _adminMaintenanceService_) {
+    inject(function(_$controller_, _$rootScope_, _adminMaintenanceService_, _adminModeService_) {
       $controller = _$controller_;
       $rootScope = _$rootScope_;
       adminMaintenanceService = _adminMaintenanceService_;
+      adminModeService = _adminModeService_;
     });
   });
 
@@ -31,15 +32,28 @@ describe('The adminMaintenanceController', function() {
   }
 
   describe('The $onInit function', function() {
-    it('should get Modules configurations from server', function() {
-      var maintenanceModules = [{ title: 'foo' }, { title: 'bar' }];
+    it('should get a filtered modules list based on current admin mode', function() {
+      var maintenanceModules = [{
+        title: 'foo',
+        maintenance: {
+          displayIn: { domain: true }
+        }
+      }, {
+        title: 'bar',
+        maintenance: {
+          displayIn: { domain: false }
+        }
+      }];
 
       adminMaintenanceService.getMaintenanceModules = sinon.stub().returns(maintenanceModules);
+      adminModeService.getCurrentMode = sinon.stub().returns('domain');
 
       var controller = initController();
 
-      expect(controller.maintenanceModules).to.shallowDeepEqual(maintenanceModules);
+      expect(controller.maintenanceModules).to.include(maintenanceModules[0]);
+      expect(controller.maintenanceModules).to.not.include(maintenanceModules[1]);
       expect(adminMaintenanceService.getMaintenanceModules).to.have.been.calledOnce;
+      expect(adminModeService.getCurrentMode).to.have.been.calledOnce;
     });
   });
 });
