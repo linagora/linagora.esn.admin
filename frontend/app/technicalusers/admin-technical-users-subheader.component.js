5f694d9e6edb6730f0f8da368222d3dbdb0f8c65
Bao Khanh LE <bkle@linagora.com>
2020-01-17 16:34:02 +0700

    esn#2826: Technical users frontend main components

---
+++ frontend/app/technicalusers/admin-technical-users-subheader.component.js
@@ -0,0 +1,10 @@
+(function(angular) {
+  'use strict';
+
+  angular.module('linagora.esn.admin')
+
+  .component('adminTechnicalUsersSubheader', {
+    templateUrl: '/admin/app/technicalusers/admin-technical-users-subheader.html'
+  });
+
+})(angular);
